d0efecfe6a8cd52689235cfdb431b20ee1ff2478
cducote <chris.ducote89@gmail.com>
2020-08-31 00:21:32 -0400

    ez CRUD

--- functions/index.js
+++ functions/index.js
@@ -3,9 +3,13 @@ const app = require('express')();
 
 const {
     getAllTodos,
-    postOneTodo
+    postOneTodo,
+    deleteTodo,
+    editTodo
 } = require('./APIs/todos')
 
 app.get('/todos', getAllTodos);
 app.post('/todo', postOneTodo)
+app.delete('/todo/:todoId', deleteTodo)
+app.put('/todo/:todoId', editTodo)
 exports.api = functions.https.onRequest(app);
